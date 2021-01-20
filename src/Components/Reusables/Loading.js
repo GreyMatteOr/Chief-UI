7fe6b2b43b2ad2f2d285e42f4f41c6e34bb04b09
Matthew Lane <themplane@gmail.com>
2021-01-19 19:01:42 -0800

    Add A reusable Loading compnt

---
+++ src/Components/Reusables/Loading.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import loadingImg from '../../images/loading.png';
+
+export default function Loading() {
+  return (
+    <div
+      className=''
+      data-testid='loading-component'
+      style={{
+        'display': 'flex',
+        'backgroundColor': 'rgba(0, 0, 0, .2)',
+        'height': '100%',
+        'position': 'relative',
+        'width': '100%',
+        'zIndex': '100'
+      }}>
+
+      <img
+        className='spin'
+        alt='loading...'
+        src={loadingImg}
+        style={{
+          'height': '10em',
+          'width': 'auto',
+          'margin': 'auto',
+          'padding': '4em'
+        }}
+      />
+
+    </div>
+  )
+}
